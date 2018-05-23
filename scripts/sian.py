230ad149b23abb9a66fa67c7ad7ce16e3e9a2841
Alocias <alocia0@gmail.com>
2018-05-23 11:19:18 +0200

    added temperature different results for SIAN, moved all table cpations to top

--- scripts/sian.py
+++ scripts/sian.py
@@ -1,8 +1,7 @@
 import numpy as np
 
-def sian(M, f, x0):
+def sian(M, f, x0, Tmax=100.):
     xold = np.array(x0)
-    Tmax = 100.
     T = Tmax
     for i in range(1,M):
         xnew = np.random.normal(xold, 1.0)
@@ -20,8 +19,8 @@ ackelyn = lambda x: -20*np.exp(-0.2*np.sqrt(0.5*(x[0]**2+x[1]**2))) -\
 rastrigin = lambda x: 20 + x[0]**2 - 10*np.cos(2*np.pi*x[0]) + x[1]**2 - \
         10*np.cos(2*np.pi*x[1])
 
-# xm, fm = sian(100000, ackelyn, (-0.01,0.01))
-xm, fm = sian(100000, rastrigin, (-0.01,0.001))
+# xm, fm = sian(100000, ackelyn, (-0.01,0.01), 50.)
+xm, fm = sian(100000, rastrigin, (-0.01,0.001), 50.)
 
 #     np.random.seed(2)
 #     np.random.seed(2)
